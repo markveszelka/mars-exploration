b65a1a00b7be578c80660ca504575e0c3a698948
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-08-09 13:54:17 +0200

    Implement 2 of 3 methods of CoordinateCalc, and write unit tests

--- src/main/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImpl.java
+++ src/main/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImpl.java
@@ -2,24 +2,29 @@ package com.codecool.marsexploration.calculators.service;
 
 import com.codecool.marsexploration.calculators.model.Coordinate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CoordinateCalculatorImpl implements CoordinateCalculator {
 
     @Override
     public Coordinate getRandomCoordinate(int dimension) {
-        // dimension * dimension négyzeten belül keresek random koordinátát.
         Random random = new Random();
-        int mapSize = (int) Math.sqrt(625);
-        int x = random.nextInt(mapSize - dimension);
-        int y = random.nextInt(mapSize - dimension);
+        int x = random.nextInt(dimension);
+        int y = random.nextInt(dimension);
         return new Coordinate(x, y);
     }
 
     @Override
     public Iterable<Coordinate> getAdjacentCoordinates(Coordinate coordinate, int dimension) {
-
-        return null;
+        List<Coordinate> coordinates = new ArrayList<>();
+        for (int i = -dimension; i <= dimension; i++) {
+            for (int j = -dimension; j <= dimension; j++) {
+                coordinates.add(new Coordinate(coordinate.x() + i, coordinate.y() + j));
+            }
+        }
+        return coordinates;
     }
 
     @Override
