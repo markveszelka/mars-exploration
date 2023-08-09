9636864888d4b5c5bc32c31076b152b62af76381
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 09:31:41 +0200

    Empty coordinateCalcImpl class

--- src/main/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImpl.java
+++ src/main/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImpl.java
@@ -10,40 +10,19 @@ public class CoordinateCalculatorImpl implements CoordinateCalculator {
 
     @Override
     public Coordinate getRandomCoordinate(int dimension) {
-        Random random = new Random();
-        int x = random.nextInt(dimension);
-        int y = random.nextInt(dimension);
-        return new Coordinate(x, y);
+
+        return null;
     }
 
     @Override
     public Iterable<Coordinate> getAdjacentCoordinates(Coordinate coordinate, int dimension) {
-        List<Coordinate> adjacentCoordinates = new ArrayList<>();
 
-        for (int xOffset = -1; xOffset <= 1; xOffset++) {
-            for (int yOffset = -1; yOffset <= 1; yOffset++) {
-                int newX = coordinate.x() + xOffset;
-                int newY = coordinate.y() + yOffset;
-
-                if (newX >= 0 && newX < dimension && newY >= 0 && newY < dimension &&
-                        (xOffset != 0 || yOffset != 0)) {
-                    adjacentCoordinates.add(new Coordinate(newX, newY));
-                }
-            }
-        }
-
-        return adjacentCoordinates;
+        return null;
     }
 
     @Override
     public Iterable<Coordinate> getAdjacentCoordinates(Iterable<Coordinate> coordinates, int dimension) {
-        List<Coordinate> allAdjacentCoordinates = new ArrayList<>();
 
-        for (Coordinate coordinate : coordinates) {
-            Iterable<Coordinate> adjacentCoordinates = getAdjacentCoordinates(coordinate, dimension);
-            allAdjacentCoordinates.addAll((List<Coordinate>) adjacentCoordinates);
-        }
-
-        return allAdjacentCoordinates;
+        return coordinates;
     }
 }
