8ac3cf31b5f58bc093e4b96a133a246fe7835d83
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 13:21:40 +0200

    Add MapElementBuilderImpl logic

--- src/main/java/com/codecool/marsexploration/mapelements/service/builder/MapElementBuilderImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/builder/MapElementBuilderImpl.java
@@ -1,10 +1,25 @@
 package com.codecool.marsexploration.mapelements.service.builder;
 
+import com.codecool.marsexploration.calculators.service.DimensionCalculator;
+import com.codecool.marsexploration.calculators.service.DimensionCalculatorImpl;
 import com.codecool.marsexploration.mapelements.model.MapElement;
 
 public class MapElementBuilderImpl implements MapElementBuilder {
+
+    DimensionCalculator dimensionCalculator;
+
+    public MapElementBuilderImpl(DimensionCalculator dimensionCalculator) {
+        this.dimensionCalculator = dimensionCalculator;
+    }
+
     @Override
     public MapElement build(int size, String symbol, String name, int dimensionGrowth, String preferredLocationSymbol) {
-        return null;
+        int dimension = dimensionCalculator.calculateDimension(size, dimensionGrowth);
+        String[][] representation = new String[dimension][dimension];
+        return new MapElement(
+                representation,
+                name,
+                dimension,
+                preferredLocationSymbol);
     }
 }
