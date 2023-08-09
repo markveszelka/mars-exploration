488616e8bc6be07a1c8ca9374b3e8c17d24da046
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 19:36:37 +0200

    Implement MapGeneratorImpl

--- src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImpl.java
@@ -1,15 +1,51 @@
 package com.codecool.marsexploration.mapelements.service.generator;
 
 import com.codecool.marsexploration.calculators.service.CoordinateCalculator;
-import com.codecool.marsexploration.calculators.service.CoordinateCalculatorImpl;
-import com.codecool.marsexploration.calculators.service.DimensionCalculator;
-import com.codecool.marsexploration.calculators.service.DimensionCalculatorImpl;
 import com.codecool.marsexploration.configuration.model.MapConfiguration;
 import com.codecool.marsexploration.mapelements.model.Map;
+import com.codecool.marsexploration.mapelements.model.MapElement;
+import com.codecool.marsexploration.mapelements.service.placer.MapElementPlacer;
+
+import java.util.List;
 
 public class MapGeneratorImpl implements MapGenerator {
+
+    private final MapElementsGenerator mapElementsGenerator;
+    private final CoordinateCalculator coordinateCalculator;
+    private final MapElementPlacer mapElementPlacer;
+    private final Map map;
+
+    public MapGeneratorImpl(Map map, MapElementsGenerator mapElementsGenerator, CoordinateCalculator coordinateCalculator, MapElementPlacer mapElementPlacer) {
+        this.mapElementsGenerator = mapElementsGenerator;
+        this.coordinateCalculator = coordinateCalculator;
+        this.mapElementPlacer = mapElementPlacer;
+        this.map = map;
+    }
+
     @Override
     public Map generate(MapConfiguration mapConfig) {
-        return null;
+        List<MapElement> mapElements = (List<MapElement>) mapElementsGenerator.createAll(mapConfig);
+        System.out.println("Number of elements: " + mapElements.size());
+
+        for (MapElement mapelement : mapElements) {
+            boolean canPlaceElement = callCanPlaceElementMethod(map, coordinateCalculator, mapElementPlacer, mapelement);
+            if (canPlaceElement) {
+                mapElementPlacer.placeElement(
+                        mapelement,
+                        map.getRepresentation(),
+                        coordinateCalculator.getRandomCoordinate(mapelement.getDimension()));
+            } else {
+                callCanPlaceElementMethod(map, coordinateCalculator, mapElementPlacer, mapelement);
+            }
+        }
+
+        return map;
+    }
+
+    private static boolean callCanPlaceElementMethod(Map map, CoordinateCalculator coordinateCalculator, MapElementPlacer mapElementPlacer, MapElement mapelement) {
+        return mapElementPlacer.canPlaceElement(
+                mapelement,
+                map.getRepresentation(),
+                coordinateCalculator.getRandomCoordinate(mapelement.getDimension()));
     }
 }
