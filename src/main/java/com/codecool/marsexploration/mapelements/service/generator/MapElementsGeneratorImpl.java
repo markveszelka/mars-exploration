4731a1d3210ad5b2f0820b0671f53a247dacd445
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 09:18:55 +0200

    Add two empty interface implementation

---
+++ src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapElementsGeneratorImpl.java
@@ -0,0 +1,11 @@
+package com.codecool.marsexploration.mapelements.service.generator;
+
+import com.codecool.marsexploration.configuration.model.MapConfiguration;
+import com.codecool.marsexploration.mapelements.model.MapElement;
+
+public class MapElementsGeneratorImpl implements MapElementsGenerator {
+    @Override
+    public Iterable<MapElement> createAll(MapConfiguration mapConfig) {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImpl.java
@@ -0,0 +1,11 @@
+package com.codecool.marsexploration.mapelements.service.generator;
+
+import com.codecool.marsexploration.configuration.model.MapConfiguration;
+import com.codecool.marsexploration.mapelements.model.Map;
+
+public class MapGeneratorImpl implements MapGenerator {
+    @Override
+    public Map generate(MapConfiguration mapConfig) {
+        return null;
+    }
+}
