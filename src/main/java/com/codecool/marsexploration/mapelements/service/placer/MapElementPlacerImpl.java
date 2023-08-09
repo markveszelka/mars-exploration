8582988b5101b6417c4a315a72ba09db51300e73
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 22:55:27 +0200

    Implement MapElementPlacerImpl

--- src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
@@ -3,11 +3,29 @@ package com.codecool.marsexploration.mapelements.service.placer;
 import com.codecool.marsexploration.calculators.model.Coordinate;
 import com.codecool.marsexploration.mapelements.model.MapElement;
 
+import java.util.Arrays;
+
 public class MapElementPlacerImpl implements MapElementPlacer {
     @Override
     public boolean canPlaceElement(MapElement element, String[][] map, Coordinate coordinate) {
+        int elementDimension = element.getDimension();
+        boolean canPlace = true;
+        String[][] elementRepresentation = element.getRepresentation();
+        if (coordinate.x() >= map.length - elementDimension || coordinate.y() >= map.length - elementDimension) {
+            canPlace = false;
+        } else {
+            for (int i = 0; i < elementRepresentation.length; i++) {
+                for (int j = 0; j < elementRepresentation[i].length; j++) {
+                    if (map[coordinate.x() + i][coordinate.y() + j].equals(" ")) {
+                        map[coordinate.x() + i][coordinate.y() + j] = elementRepresentation[i][j];
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
 
-        return false;
+        return canPlace;
     }
 
     @Override
