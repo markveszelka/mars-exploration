40291725e00fccb20db3d701e38f40d230d4f9b0
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-09 13:21:59 +0200

    Add MapElementsGeneratorImpl logic

--- src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapElementsGeneratorImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapElementsGeneratorImpl.java
@@ -1,11 +1,41 @@
 package com.codecool.marsexploration.mapelements.service.generator;
 
+import com.codecool.marsexploration.configuration.model.ElementToSize;
 import com.codecool.marsexploration.configuration.model.MapConfiguration;
+import com.codecool.marsexploration.configuration.model.MapElementConfiguration;
 import com.codecool.marsexploration.mapelements.model.MapElement;
+import com.codecool.marsexploration.mapelements.service.builder.MapElementBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapElementsGeneratorImpl implements MapElementsGenerator {
+
+    MapElementBuilder mapElementFactory;
+
+    public MapElementsGeneratorImpl(MapElementBuilder mapElementBuilder) {
+        this.mapElementFactory = mapElementBuilder;
+    }
+
     @Override
     public Iterable<MapElement> createAll(MapConfiguration mapConfig) {
-        return null;
+        List<MapElement> mapElementsList = new ArrayList<>();
+        List<MapElementConfiguration> mapElementConfigurations = mapConfig.mapElementConfigurations();
+
+        for (MapElementConfiguration mapElementConfiguration : mapElementConfigurations) {
+            int dimensionGrowth = mapElementConfiguration.dimensionGrowth();
+            String name = mapElementConfiguration.name();
+            String symbol = mapElementConfiguration.symbol();
+            String preferredLocationSymbol = mapElementConfiguration.preferredLocationSymbol();
+            List<ElementToSize> elementToSizesList = mapElementConfiguration.elementToSizes();
+
+            for (ElementToSize element : elementToSizesList) {
+                int size = element.size();
+                MapElement mapElement = mapElementFactory.build(size, symbol, name, dimensionGrowth, preferredLocationSymbol);
+                mapElementsList.add(mapElement);
+            }
+        }
+
+        return mapElementsList;
     }
 }
