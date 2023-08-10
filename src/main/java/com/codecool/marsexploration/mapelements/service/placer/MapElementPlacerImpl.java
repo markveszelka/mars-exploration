b4078ee97c12097949cc551463291a1c13a442e6
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-10 18:54:22 +0200

    Rewrite canPlaceElement method in MapElementPlacerImpl

--- src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
@@ -8,31 +8,20 @@ import java.util.Arrays;
 public class MapElementPlacerImpl implements MapElementPlacer {
     @Override
     public boolean canPlaceElement(MapElement element, String[][] map, Coordinate coordinate) {
-        System.out.println(coordinate);
         int elementDimension = element.getDimension();
-        boolean canPlace = true;
-        String[][] elementRepresentation = element.getRepresentation();
-
-        if (coordinate.x() > map.length - elementDimension || coordinate.y() > map.length - elementDimension) {
-            canPlace = false;
-        } else {
-            for (int i = 0; i < elementRepresentation.length; i++) {
-                for (int j = 0; j < elementRepresentation[i].length; j++) {
-                    if (map[coordinate.x() + i][coordinate.y() + j].equals(" ")) {
-                        map[coordinate.x() + i][coordinate.y() + j] = elementRepresentation[i][j];
-                    } else {
-                        canPlace = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return canPlace;
+        return coordinate.x() <= map.length - elementDimension && coordinate.y() <= map.length - elementDimension;
     }
 
     @Override
     public void placeElement(MapElement element, String[][] map, Coordinate coordinate) {
+        String[][] elementRepresentation = element.getRepresentation();
 
+        for (int i = 0; i < elementRepresentation.length; i++) {
+            for (int j = 0; j < elementRepresentation[i].length; j++) {
+                if (map[coordinate.x() + i][coordinate.y() + j].equals(" ")) {
+                    map[coordinate.x() + i][coordinate.y() + j] = elementRepresentation[i][j];
+                }
+            }
+        }
     }
 }
